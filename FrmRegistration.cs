39b3b98b6ea3cde648996241da8d7dd524ad2ba3
Marc Rhupert Cleofas <116242246+skeminanat@users.noreply.github.com>
2022-11-24 15:10:36 +0800

    Add files via upload

---
+++ FrmRegistration.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ReadingAFile_ChallengeExer
+{
+    public partial class FrmRegistration : Form
+    {
+        public static string SetFileName;
+        public FrmRegistration()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            SetFileName = txtStudentNo.Text + ".txt";
+
+            string getStudentNo = "Student No.: " + txtStudentNo.Text;
+            string getFullName = "FullName No.: " + txtLastName.Text + " " + txtFirstName.Text + " " + txtMI.Text;
+            string getProgram = "Program: " + cbProgram.Text;
+            string getGender = "Gender: " + cbGender.Text;
+            string getAge = "Age: " + txtAge.Text;
+            string getBirthday = "Birthday: " + dtpBirthday.Text;
+            string getContactNo = "Contact No.: " + txtContactNo.Text;
+
+
+            string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath,
+            SetFileName)))
+            {
+                outputFile.WriteLine(getStudentNo);
+                Console.WriteLine(getStudentNo);
+                outputFile.WriteLine(getFullName);
+                Console.WriteLine(getFullName);
+                outputFile.WriteLine(getProgram);
+                Console.WriteLine(getProgram);
+                outputFile.WriteLine(getGender);
+                Console.WriteLine(getGender);
+                outputFile.WriteLine(getAge);
+                Console.WriteLine(getAge);
+                outputFile.WriteLine(getBirthday);
+                Console.WriteLine(getBirthday);
+                outputFile.WriteLine(getContactNo);
+                Console.WriteLine(getContactNo);
+            }
+        }
+
+        private void btnRecords_Click(object sender, EventArgs e)
+        {
+            FrmStudentRecord record = new FrmStudentRecord();
+            record.Show();
+            this.Close();
+        }
+    }
+}
---
+++ FrmStudentRecord.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReadingAFile_ChallengeExer
+{
+    public partial class FrmStudentRecord : Form
+    {
+        public FrmStudentRecord()
+        {
+            InitializeComponent();
+        }
+
+        void DisplayToList()
+        {
+            openFileDialog1.InitialDirectory = @"MyDocuments";
+            openFileDialog1.Title = "Browse Text File";
+            openFileDialog1.DefaultExt = ".txt";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.ShowDialog();
+
+            string path = openFileDialog1.FileName;
+            using (StreamReader streamReader = File.OpenText(path))
+            {
+                string _getText = "";
+                while ((_getText = streamReader.ReadLine()) != null)
+                {
+                    Console.WriteLine(_getText);
+                    lvViewRecord.Items.Add(_getText);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FrmRegistration reg = new FrmRegistration();
+            reg.Show();
+            this.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DisplayToList();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            lvViewRecord.Clear();
+            MessageBox.Show("Successfully Uploaded!");
+        }
+    }
+}
